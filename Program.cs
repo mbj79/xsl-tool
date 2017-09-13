b8936634e247b37e454baaef1e1cde600bc9f41f
Marcus Björklund <mbj79@users.noreply.github.com>
2017-09-13 11:57:55 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace Ferrologic.Tools.Xsl
+{
+    internal class Program
+    {
+        private const int EXIT_CODE_OK = 0;
+        private const int EXIT_CODE_INCORRECT_USAGE = 1;
+        private const int EXIST_CODE_TRANSFORMATION_FAILURE = 2;
+        private const int EXIT_CODE_REINDENT_FAILURE = 3;
+
+        private static int Main(string[] args)
+        {
+            int exitCode = EXIT_CODE_INCORRECT_USAGE;
+
+            try
+            {
+                if (!UsageOk(args))
+                    return EXIT_CODE_INCORRECT_USAGE;
+
+                var inputPath = args[0];
+                var transformPath = args[1];
+                var outputPath = args[2];
+
+                exitCode = EXIST_CODE_TRANSFORMATION_FAILURE;
+
+                var settings = new XsltSettings { EnableDocumentFunction = true, EnableScript = false };
+                var transform = new XslCompiledTransform();
+                var xmlResolver = new XmlUrlResolver();
+                var xsltArgumentList = new XsltArgumentList();
+
+                for (int i = 3; i < args.Length; i++)
+                {
+                    var keyValuePair = args[i].Split('=');
+                    xsltArgumentList.AddParam(keyValuePair[0], "", keyValuePair[1]);
+                }
+
+                using (var outputStream = new StreamWriter(outputPath))
+                {
+                    transform.Load(transformPath, settings, xmlResolver);
+                    transform.Transform(inputPath, xsltArgumentList, outputStream);
+                }
+
+                if (transform.OutputSettings.OutputMethod == XmlOutputMethod.Text) return EXIT_CODE_OK;
+
+                exitCode = EXIT_CODE_REINDENT_FAILURE;
+                Reindent(outputPath);
+            }
+            catch (Exception ex)
+            {
+                while (ex != null)
+                {
+                    Console.Error.WriteLine("Error: " + ex);
+                    ex = ex.InnerException;
+                }
+                return exitCode;
+            }
+            return EXIT_CODE_OK;
+        }
+
+        private static bool UsageOk(string[] args)
+        {
+            if (args.Length >= 3)
+                return true;
+
+            Console.WriteLine("Usage: {0} inputPath transformPath outputPath [param1=value1 [param2=value2 [...]]]",
+                              Path.GetFileName(Assembly.GetExecutingAssembly().CodeBase));
+            Console.WriteLine();
+            return false;
+        }
+
+        private static void Reindent(string path)
+        {
+            var xml = new XmlDocument();
+            xml.Load(path);
+
+            var settings = new XmlWriterSettings { Indent = true, IndentChars = "\t" };
+            var xmlWriter = XmlWriter.Create(path, settings);
+
+            xml.PreserveWhitespace = false;
+            xml.Save(xmlWriter);
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ferrologic XSL Tool")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ferrologic")]
+[assembly: AssemblyProduct("Ferrologic Tools")]
+[assembly: AssemblyCopyright("Copyright © Ferrologic 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9075178c-2c01-4bb6-abf6-313f8b4af0cf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
